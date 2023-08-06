b492a22a49d05df10cabaff7e7ee1373b8be3bb3
piggy <babymechanic@gmail.com>
2023-08-06 21:26:53 +1000

    putting type check tests inside it blocks

--- test/type-manipulation/index.test.ts
+++ test/type-manipulation/index.test.ts
@@ -1,37 +1,48 @@
 import { DeepRequired, ExtractByProp, ExtractLast, PartialExcept } from '../../src/type-manipulation';
 import { Equal, Expect } from '../test-utils/type-assertions';
-import { describe } from 'vitest';
-
+import { describe, it } from 'vitest';
 
 describe('ExtractByProp', () => {
-    type Result = { status: 'success' } | { status: 'error' };
 
-    type Cases = [
-        Expect<Equal<ExtractByProp<'status', 'success', Result>, { status: 'success' }>>,
-        Expect<Equal<ExtractByProp<'status', 'error', Result>, { status: 'error' }>>,
-    ]
+    it('should extract items based on the discriminator prop', () => {
+        type Result = { status: 'success' } | { status: 'error' };
+
+        type Cases = [
+            Expect<Equal<ExtractByProp<'status', 'success', Result>, { status: 'success' }>>,
+            Expect<Equal<ExtractByProp<'status', 'error', Result>, { status: 'error' }>>,
+        ]
+    });
+
 });
 
 describe('PartialExcept', () => {
-    type TestData = { optional?: string; mandatory: string; };
+    it('should make everything optional but make the specified props mandatory', () => {
+        type TestData = { optional?: string; mandatory: string; };
 
-    type Created = PartialExcept<TestData, 'optional'>;
+        type Created = PartialExcept<TestData, 'optional'>;
 
-    type Cases = [
-        Expect<Equal<Created, { optional: string; mandatory: string | undefined; }>>
-    ]
+        type Cases = [
+            Expect<Equal<Created, { optional: string; mandatory: string | undefined; }>>
+        ]
+
+    });
 });
 
 describe('DeepRequired', () => {
-    type Cases = [Expect<Equal<
-        DeepRequired<{ grandParent?: { parent?: { child?: string | undefined } } }>,
-        { grandParent: { parent: { child: string } } }
-    >>]
+    it('should make the entire object required recursively', () => {
+        type Cases = [Expect<Equal<
+            DeepRequired<{ grandParent?: { parent?: { child?: string | undefined } } }>,
+            { grandParent: { parent: { child: string } } }
+        >>]
+
+    });
 });
 
 describe('ExtractLast', () => {
-    type Cases = [
-        Expect<Equal<ExtractLast<[string, number, Date]>, Date>>
-    ];
+    it('should extract the last type from an array', function () {
+        type Cases = [
+            Expect<Equal<ExtractLast<[string, number, Date]>, Date>>
+        ];
+    });
 });
 
