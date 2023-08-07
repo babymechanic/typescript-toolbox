4561d2634cb4407b95a94c73411dd1a5c2d82a15
piggy <babymechanic@gmail.com>
2023-08-07 16:05:00 +1000

    fixing module exports

--- src/discriminated-union/handle.ts
+++ src/discriminated-union/handle.ts
@@ -2,8 +2,14 @@ import { ExtractByProp } from '../type-manipulation';
 
 type ExtractStatus<TProp extends string, T extends { [K in TProp]: string }> = T[TProp];
 
-export const createHandler = <TProp extends string, TDiscriminatedUnion extends { [K in TProp]: string }, TReturn>(prop: TProp, handlers: {
+type Handlers<TProp extends string, TDiscriminatedUnion extends { [K in TProp]: string }, TReturn> = {
     [K in ExtractStatus<TProp, TDiscriminatedUnion>]: (val: ExtractByProp<TProp, K, TDiscriminatedUnion>) => TReturn
-}) => (value: TDiscriminatedUnion) => {
+};
+
+export const createHandler = <
+    TProp extends string,
+    TDiscriminatedUnion extends { [K in TProp]: string },
+    TReturn
+>(prop: TProp, handlers: Handlers<TProp, TDiscriminatedUnion, TReturn>) => (value: TDiscriminatedUnion): TReturn => {
     return handlers[value[prop]](value as ExtractByProp<TProp, TDiscriminatedUnion[TProp], TDiscriminatedUnion>);
 };
