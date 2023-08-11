cef8823eee91df895468d1b9bee9b6ce76eea9a0
piggy <babymechanic@gmail.com>
2023-08-11 17:15:26 +1000

    fixing type

--- src/discriminated-union/event-emitter.ts
+++ src/discriminated-union/event-emitter.ts
@@ -4,28 +4,28 @@ type Event = {
     readonly name: string;
 }
 
-export type EventEmitter<TEvent extends Event, TEventName extends string = TEvent['name']> = {
-    on<T extends TEventName>(name: T, callable: (payload: ExtractByProp<'name', T, TEvent>) => Promise<void>): () => void;
+export type EventEmitter<TEvent extends Event> = {
+    on<T extends TEvent['name']>(name: T, callable: (payload: ExtractByProp<'name', T, TEvent>) => Promise<void>): () => void;
     emit<T extends TEvent>(payload: T): Promise<void>;
 }
 
-export const createEventEmitter = <TEvent extends Event, TEventName extends string = TEvent['name']>(): EventEmitter<TEvent, TEventName> => {
+export const createEventEmitter = <TEvent extends Event>(): EventEmitter<TEvent> => {
 
     const subscribers: Partial<{
-        [K in TEventName]: ((payload: ExtractByProp<'name', K, TEvent>) => Promise<void>)[]
+        [K in TEvent['name']]: ((payload: ExtractByProp<'name', K, TEvent>) => Promise<void>)[]
     }> = {};
 
     return {
-        on<T extends TEventName>(name: T, callable: (payload: ExtractByProp<'name', T, TEvent>) => Promise<void>): () => void {
+        on<T extends TEvent['name']>(name: T, callable: (payload: ExtractByProp<'name', T, TEvent>) => Promise<void>): () => void {
             subscribers[name] = (subscribers[name] || []).concat(callable);
             return () => {
                 subscribers[name] = (subscribers[name] || []).filter(x => x !== callable);
             }
         },
         async emit<const T extends TEvent>(payload: T): Promise<void> {
-            const eventName = payload.name as TEventName;
+            const eventName = payload.name as TEvent['name'];
             type Payload<TEvent extends Event> = ExtractByProp<'name', typeof eventName, TEvent>;
-            Promise
+            return Promise
                 .all((subscribers[eventName] || []).map(x => x(payload as Payload<TEvent>)))
                 .then();
         }
