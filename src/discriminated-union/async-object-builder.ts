6529511dbf3ee7b7e18ba69ed4d721806deaed09
piggy <babymechanic@gmail.com>
2024-07-17 21:55:04 +1000

    add initial builder

--- src/discriminated-union/async-object-builder.ts
+++ src/discriminated-union/async-object-builder.ts
@@ -3,20 +3,24 @@ import { InvalidConfigurationError } from '../errors/invalid-configuration-error
 
 type Failure = {
     type: 'failure';
-    error: unknown;
+    details: unknown;
 }
 
 type Success<T> = {
     type: 'success';
-    value: T
+    value: T;
 };
 
 export type CreateResult<TOutPut> = Success<TOutPut> | Failure;
 export type BuilderFunc<TInput, TOutPut> = (buildDeps: TInput) => Promise<CreateResult<TOutPut>>;
+type BuilderConfig<TInput, TOutput> = {
+    builder: BuilderFunc<TInput, TOutput>;
+    dispose?: (output: TOutput) => void;
+};
 
 type ExtractObject<T> = {
     [K in keyof T]:
-    T[K] extends BuilderFunc<unknown, infer TOutput> ? TOutput : never;
+    T[K] extends BuilderConfig<unknown, infer TOutput> ? TOutput : never;
 }
 
 type AggregateFailure<TKey extends PropertyKey> = Failure & {
@@ -24,45 +28,85 @@ type AggregateFailure<TKey extends PropertyKey> = Failure & {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type BuildConfig = { [K in PropertyKey]: BuilderFunc<any, any> }
+type BuildConfig = { [K in PropertyKey]: BuilderConfig<any, any> }
 
+const safeDispose = (disposers: (() => void)[]): unknown[] => {
+    return disposers.reverse().reduce<unknown[]>((acc, disposer) => {
+        try {
+            disposer()
+        } catch (e) {
+            console.error(e);
+            acc.push(e);
+        }
+        return acc;
+    }, []);
+};
 
-const asyncObjectBuilder = <TBuildConfiguration extends BuildConfig = Record<string, never>>(buildConfig: TBuildConfiguration, propBuildOrder: (keyof TBuildConfiguration)[]) => {
+export type ObjectBuilderOptions = {
+    dispose: (disposers: (() => void)[]) => unknown[];
+}
+
+const _asyncObjectBuilder = <
+    TBuildConfiguration extends BuildConfig = Record<string, never>
+>(
+    buildConfig: TBuildConfiguration,
+    propBuildOrder: (keyof TBuildConfiguration)[],
+    options: ObjectBuilderOptions
+) => {
     type SuccessfullyBuiltObject = ExtractObject<TBuildConfiguration>;
     type AggregateResult =
         AggregateFailure<keyof TBuildConfiguration>
-        | Success<SuccessfullyBuiltObject>;
+        | (Success<SuccessfullyBuiltObject> & { dispose: () => unknown[]; });
 
     return {
-        with: <TProp extends PropertyKey, TFunc extends BuilderFunc<Omit<SuccessfullyBuiltObject, TProp>, unknown>>(prop: TProp, func: TFunc) => {
+        with: <
+            TProp extends PropertyKey,
+            TBuilderConfig extends BuilderConfig<Omit<SuccessfullyBuiltObject, TProp>, unknown>
+        >(prop: TProp, config: TBuilderConfig) => {
             if (propBuildOrder.includes(prop)) {
                 throw new InvalidConfigurationError(`'${ prop.toString() }' prop was defined twice`)
             }
-            type UpdateConfig = AddProp<TBuildConfiguration, TProp, TFunc>
+            type UpdatedConfig = AddProp<TBuildConfiguration, TProp, TBuilderConfig>
             const updatedConfig = {
                 ...buildConfig,
-                [prop]: func
-            } as UpdateConfig;
-            return asyncObjectBuilder(updatedConfig, [...propBuildOrder, prop] as (keyof UpdateConfig)[])
+                [prop]: config
+            } as UpdatedConfig;
+            return _asyncObjectBuilder(updatedConfig, [...propBuildOrder, prop] as (keyof UpdatedConfig)[], options)
         },
 
         build: async (): Promise<AggregateResult> => {
             const acc: Partial<SuccessfullyBuiltObject> = {};
+            const disposers: (() => void)[] = [];
             for (const prop of propBuildOrder) {
-                const builder = buildConfig[prop];
-                const result = await builder(acc);
-                if (result.type === 'success') {
-                    acc[prop] = result.value as SuccessfullyBuiltObject[typeof prop];
-                } else {
-                    return { ...result, prop };
+                const { builder, dispose } = buildConfig[prop];
+                try {
+                    const result = await builder(acc);
+                    if (result.type === 'success') {
+                        acc[prop] = result.value as SuccessfullyBuiltObject[typeof prop];
+                        if (dispose != null) {
+                            disposers.push(() => dispose(result.value));
+                        }
+                    } else {
+                        options.dispose(disposers);
+                        return { ...result, prop };
+                    }
+                } catch (e) {
+                    options.dispose(disposers);
+                    return {
+                        type: 'failure',
+                        prop,
+                        details: e
+                    }
                 }
             }
             return {
                 type: 'success',
-                value: acc as SuccessfullyBuiltObject
+                value: acc as SuccessfullyBuiltObject,
+                dispose: () => options.dispose(disposers)
             }
         }
     }
 };
 
-export const createAsyncObjectBuilder = () => asyncObjectBuilder({}, []);
+export const asyncObjectBuilder = (options: ObjectBuilderOptions = { dispose: safeDispose }) =>
+    _asyncObjectBuilder({}, [], options);
--- test/discriminated-union/async-object-builder.test.ts
+++ test/discriminated-union/async-object-builder.test.ts
@@ -1,13 +1,14 @@
 import { describe, expect, it, vi } from 'vitest';
-import { createAsyncObjectBuilder } from '../../src/discriminated-union/async-object-builder';
+import { asyncObjectBuilder } from '../../src/discriminated-union/async-object-builder';
 import { InvalidConfigurationError } from '../../src/errors/invalid-configuration-error';
+import { ExtractByProp } from '../../src/type-manipulation';
 
 describe("createAsyncObjectBuilder", () => {
 
     it('should be able to create an object', async () => {
-        const config = createAsyncObjectBuilder()
-            .with('num', async () => ({ type: 'success', value: 123123 }))
-            .with('str', async () => ({ type: 'success', value: 'hello world' }));
+        const config = asyncObjectBuilder()
+            .with('num', { builder: async () => ({ type: 'success', value: 123123 }) })
+            .with('str', { builder: async () => ({ type: 'success', value: 'hello world' }) });
 
         const obj = await config.build()
 
@@ -16,14 +17,15 @@ describe("createAsyncObjectBuilder", () => {
             value: {
                 num: 123123,
                 str: 'hello world'
-            }
+            },
+            dispose: expect.any(Function)
         });
     });
 
     it('should be able to pass the previously built value to the next builder', async () => {
-        const config = createAsyncObjectBuilder()
-            .with('num', async () => ({ type: 'success', value: 123123 }))
-            .with('str', async ({ num }) => ({ type: 'success', value: num.toString() }));
+        const config = asyncObjectBuilder()
+            .with('num', { builder: async () => ({ type: 'success', value: 123123 }) })
+            .with('str', { builder: async ({ num }) => ({ type: 'success', value: num.toString() }) });
 
         const obj = await config.build()
 
@@ -32,43 +34,44 @@ describe("createAsyncObjectBuilder", () => {
             value: {
                 num: 123123,
                 str: '123123'
-            }
+            },
+            dispose: expect.any(Function)
         });
     });
 
     it('should return error if first function returns a failure', async () => {
-        const config = createAsyncObjectBuilder()
-            .with('num', async () => ({ type: 'failure', error: 'first error' }))
-            .with('str', async () => ({ type: 'success', value: 'this should not be called' }));
+        const config = asyncObjectBuilder()
+            .with('num', { builder: async () => ({ type: 'failure', details: 'first error' }) })
+            .with('str', { builder: async () => ({ type: 'success', value: 'this should not be called' }) });
 
         const obj = await config.build()
 
         expect(obj).toEqual({
             type: 'failure',
-            error: 'first error',
+            details: 'first error',
             prop: 'num'
         });
     });
 
     it('should return error if second function returns a failure', async () => {
-        const config = createAsyncObjectBuilder()
-            .with('num', async () => ({ type: 'success', value: 123123 }))
-            .with('str', async () => ({ type: 'failure', error: 'second error' }));
+        const config = asyncObjectBuilder()
+            .with('num', { builder: async () => ({ type: 'success', value: 123123 }) })
+            .with('str', { builder: async () => ({ type: 'failure', details: 'second error' }) });
 
         const obj = await config.build()
 
         expect(obj).toEqual({
             type: 'failure',
-            error: 'second error',
+            details: 'second error',
             prop: 'str'
         });
     });
 
     it('should not call second builder if first one fails', async () => {
         const mock = vi.fn();
-        const config = createAsyncObjectBuilder()
-            .with('num', async () => ({ type: 'failure', error: 'first error' }))
-            .with('str', mock);
+        const config = asyncObjectBuilder()
+            .with('num', { builder: async () => ({ type: 'failure', details: 'first error' }) })
+            .with('str', { builder: mock });
 
         await config.build()
 
@@ -76,9 +79,96 @@ describe("createAsyncObjectBuilder", () => {
     });
 
     it('throws error if the same prop is defined twice', async () => {
-        expect(() => createAsyncObjectBuilder()
-            .with('num', async () => ({ type: 'success', value: 123123 }))
-            .with('num', async () => ({ type: 'success', value: 123123 }))
+        expect(() => asyncObjectBuilder()
+            .with('num', { builder: async () => ({ type: 'success', value: 123123 }) })
+            .with('num', { builder: async () => ({ type: 'success', value: 123123 }) })
         ).toThrow(new InvalidConfigurationError("'num' prop was defined twice"));
     });
+
+    it('calls all the dispose methods on the built object', async () => {
+        const dispose1 = vi.fn();
+        const dispose2 = vi.fn();
+        const result = await asyncObjectBuilder()
+            .with('num', {
+                builder: async () => ({ type: 'success', value: 123123 }),
+                dispose: dispose1
+            })
+            .with('str', {
+                builder: async () => ({ type: 'success', value: 'hello world' }),
+                dispose: dispose2
+            }).build();
+        const successResult = result as ExtractByProp<'type', 'success', typeof result>;
+
+        successResult.dispose();
+
+        expect(dispose1).toHaveBeenCalledWith(123123);
+        expect(dispose2).toHaveBeenCalledWith('hello world');
+    });
+
+    it('should not let any errors bubble on dispose', async () => {
+        const firstError = new Error("first dispose error");
+        const secondError = new Error("second dispose error");
+        const result = await asyncObjectBuilder()
+            .with('num', {
+                builder: async () => ({ type: 'success', value: 123123 }),
+                dispose: vi.fn().mockImplementation(() => {
+                    throw firstError;
+                })
+            })
+            .with('str', {
+                builder: async () => ({ type: 'success', value: 'hello world' }),
+                dispose: vi.fn().mockImplementation(() => {
+                    throw secondError;
+                })
+            }).build();
+        const successResult = result as ExtractByProp<'type', 'success', typeof result>;
+
+        const disposed = successResult.dispose();
+
+        expect(disposed).toEqual([secondError, firstError]);
+    });
+
+    it('should dispose what ever was built if there is a failure', async () => {
+        const secondError = new Error("second error");
+        const firstDispose = vi.fn();
+        const config = asyncObjectBuilder()
+            .with('num', {
+                builder: async () => ({ type: 'success', value: 123123 }),
+                dispose: firstDispose
+            })
+            .with('str', {
+                builder:  () => Promise.reject(secondError),
+            });
+
+        const result = await config.build();
+
+        expect(result).toEqual({
+            type: 'failure',
+            details: secondError,
+            prop: 'str'
+        });
+        expect(firstDispose).toHaveBeenCalledWith(123123);
+    });
+
+    it('should dispose what ever was built if one of the builders returns an error', async () => {
+        const firstDispose = vi.fn();
+        const config = asyncObjectBuilder()
+            .with('num', {
+                builder: async () => ({ type: 'success', value: 123123 }),
+                dispose: firstDispose
+            })
+            .with('str', {
+                builder:  async () => ({ type: 'failure', details: 'could not build' }),
+            });
+
+        const result = await config.build();
+
+        expect(result).toEqual({
+            type: 'failure',
+            details: 'could not build',
+            prop: 'str'
+        });
+        expect(firstDispose).toHaveBeenCalledWith(123123);
+    });
+
 });
